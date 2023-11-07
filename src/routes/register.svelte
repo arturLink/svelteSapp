5242525f4a3d3369daedd9010a8fb5b75de61240
opilane <arturlink04@gmail.ocm>
2023-11-07 12:01:38 +0200

    Done

---
+++ src/routes/register.svelte
@@ -0,0 +1,68 @@
+<!--
+<script context="module">
+    export async function preload({ params }, { user }) {
+        if (user) {
+            this.redirect(302, `/`);
+        }
+    }
+</script>
+-->
+<script>
+    import { goto, stores } from '@sapper/app';
+    import { post } from 'utils.js';
+
+    const { session } = stores();
+
+    let username = '';
+    let name = '';
+    let password = '';
+    let error = null;
+
+    async function submit(event) {
+        const response = await post(`auth/register`, { username, name, password });
+
+        // TODO handle network errors
+        error = response.error;
+
+        if (response.id) {
+
+            goto('/');
+        }
+    }
+</script>
+
+<svelte:head>
+    <title>Sign up • barbank</title>
+</svelte:head>
+
+<div class="auth-page">
+    <div class="container page">
+        <div class="row">
+            <div class="col-md-6 offset-md-3 col-xs-12">
+                <h1 class="text-xs-center">Sign up</h1>
+                <p class="text-xs-center">
+                    <a href="/login">Have an account?</a>
+                </p>
+
+                {#if error}
+                    <div class="alert alert-danger" role="alert">{error}</div>
+                {/if}
+                <form on:submit|preventDefault={submit}>
+                    <fieldset class="form-group">
+                        <input class="form-control form-control-lg" type="text" required placeholder="Your Username" bind:value={username}>
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <input class="form-control form-control-lg" type="text" required placeholder="Name" bind:value={name}>
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <input class="form-control form-control-lg" type="password" required placeholder="Password" bind:value={password}>
+                        {#if password.length > 0 && password.length <7}<sup><div class="alert alert-danger" role="alert">Password too short</div></sup>{/if}
+                    </fieldset>
+                    <button class="btn btn-lg btn-primary pull-xs-right" disabled='{password.length < 7}'>
+                        Sign up
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
